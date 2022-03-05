7cf0632628ee0e1a379feb0cd9f4d47ea3cc8fc1
JosselinSomervilleRoberts <josselin.somerville@gmail.com>
2022-03-05 19:42:41 +0100

    Add Perlin Noise in the shader

--- shaders/mesh/vert.glsl
+++ shaders/mesh/vert.glsl
@@ -30,6 +30,52 @@ int N_waves = 1;
 uniform float t;
 
 
+
+
+// ====================== PERLIN NOISE =========================== //
+float PI = 3.1415f;
+float screenWidth = 10.0f;
+
+float rand(vec2 c){
+	return fract(sin(dot(c.xy ,vec2(12.9898,78.233))) * 43758.5453);
+}
+
+float noise(vec2 p, float freq ){
+	float unit = screenWidth/freq;
+	vec2 ij = floor(p/unit);
+	vec2 xy = mod(p,unit)/unit;
+	//xy = 3.*xy*xy-2.*xy*xy*xy;
+	xy = .5*(1.-cos(PI*xy));
+	float a = rand((ij+vec2(0.,0.)));
+	float b = rand((ij+vec2(1.,0.)));
+	float c = rand((ij+vec2(0.,1.)));
+	float d = rand((ij+vec2(1.,1.)));
+	float x1 = mix(a, b, xy.x);
+	float x2 = mix(c, d, xy.x);
+	return mix(x1, x2, xy.y);
+}
+
+float pNoise(vec2 p, int res){
+	float persistance = .5;
+	float n = 0.;
+	float normK = 0.;
+	float f = 4.;
+	float amp = 1.;
+	int iCount = 0;
+	for (int i = 0; i<50; i++){
+		n+=amp*noise(p, f);
+		f*=2.;
+		normK+=amp;
+		amp*=persistance;
+		if (iCount == res) break;
+		iCount++;
+	}
+	float nf = n/normK;
+	return nf*nf*nf*nf;
+}
+// =================================================================== //
+
+
 float wave_arg(float a, float f, vec2 dir, float u, float v) {
     return f*t - (dir[0]*u + dir[1]*v);
 }
@@ -49,6 +95,7 @@ vec3 compute_wave_pos(vec3 pos)
         X += a * normalize(dir) * sin(arg);
         Z += a * cos(arg);
     }
+	Z+= pNoise(vec2(position), 1);
 
     return vec3(X[0], X[1], Z);
 }
